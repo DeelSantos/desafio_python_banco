614f8b7e9001665eae5660495bccf630793f63e9
Edelson Santos <edelsonjr.4@hotmail.com>
2024-04-01 21:48:46 -0300

    Update sistema_bancário.py

--- sistema_bancário.py
+++ sistema_bancário.py
@@ -13,6 +13,29 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+def sacar_dinheiro (saldo, limite, extrato, numero_saques, LIMITE_SAQUES):
+    valor = float(input("Qual valor de saque? "))
+        
+    excedeu_saldo = valor > saldo
+        
+    excedeu_limite = valor > limite
+        
+    excedeu_saques = numero_saques >= LIMITE_SAQUES
+        
+    if excedeu_saldo:
+        print("Seu saque excedeu o saldo, tente novamente!")
+    elif excedeu_limite:
+        print("Seu saque excedeu o limite possível, tente novamente!")
+        
+    elif excedeu_saques:
+        print("Seu saque excedeu o limite de saques, favor tente novamente amanhã!")
+    else:
+        saldo = saldo - valor
+        extrato += f"Saque realizado de: {valor:.2f}\n"
+        numero_saques += 1
+    return saldo
+
+
 while True:
     
     
@@ -34,25 +57,7 @@ while True:
     elif opcao == "s":
         
         print("******************Sacar***********************")
-        valor = float(input("Qual valor de saque? "))
-        
-        excedeu_saldo = valor > saldo
-        
-        excedeu_limite = valor > limite
-        
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
-        
-        if excedeu_saldo:
-            print("Seu saque excedeu o saldo, tente novamente!")
-        elif excedeu_limite:
-            print("Seu saque excedeu o limite possível, tente novamente!")
-        
-        elif excedeu_saques:
-            print("Seu saque excedeu o limite de saques, favor tente novamente amanhã!")
-        else:
-            saldo = saldo - valor
-            extrato += f"Saque realizado de: {valor:.2f}\n"
-            numero_saques += 1
+        sacar_dinheiro(saldo, limite, extrato, numero_saques, LIMITE_SAQUES)
             
             
         
